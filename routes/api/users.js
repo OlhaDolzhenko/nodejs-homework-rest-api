3510af67cdab8e1e5ce7ef3e374b6c8f9fdd5f88
Olha Dolzhenko <olyasorokolat@gmail.com>
2022-02-23 19:08:26 -0500

    avatar upload and update

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const { fstat } = require("fs");
 const createError = require('http-errors');
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require('jimp');
 
-const { authenticate } = require("../../middlewares");
+const { authenticate, upload } = require("../../middlewares");
 
 const { User, schemas } = require("../../models/user");
 
@@ -31,6 +35,32 @@ router.patch("/", authenticate, async (req, res, next) => {
             throw new createError(404, "Not found")
         }
         res.json(result);
+    } catch (error) {
+        next(error);
+    }
+})
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+const optimizeImg = async (imagePath) => {
+  const image = await Jimp.read(`${imagePath}`);
+  await image.resize(250, 250);
+  return await image.writeAsync(`${imagePath}`);
+};
+
+router.patch("/avatars", authenticate, upload.single("avatar"), async (req, res, next) => {
+    const { _id } = req.user;
+    const { path: tempUpload, filename } = req.file;
+    try {
+        const [extention] = filename.split(".").reverse();
+        const newFileName = `${_id}.${extention}`;
+        const resultUpload = path.join(avatarsDir, newFileName);
+        await optimizeImg(filename);
+        await fs.rename(tempUpload, resultUpload);
+        const avatarURL = path.join("avatars", newFileName);
+        await User.findByIdAndUpdate(_id, { avatarURL });
+        res.json({
+            avatarURL
+        })
     } catch (error) {
         next(error);
     }
